92e5524f1a52e0d4419ad1010a6c5797709d7654
JianchengHe <1019015673@qq.com>
2020-12-28 11:11:20 +0800

    xiugai

---
+++ models/bitcoin_command.go
@@ -0,0 +1,53 @@
+package models
+
+//获取
+type GetBlock struct {
+	Hash          string   `json:"hash"`
+	Confirmations int64    `json:"confirmations"`
+	Strippedsize  int64    `json:"strippedsize"`
+	Size          int64    `json:"size"`
+	Weight        int64    `json:"weight"`
+	Height        int64    `json:"height"`
+	Version       int64    `json:"version"`
+	VersionHex    string   `json:"version_hex"`
+	Merkleroot    string   `json:"merkleroot"`
+	Tx            []string `json:"merkleroot"`
+	Time          int64    `json:"time"`
+	Mediantime    int64    `json:"mediantime"`
+	Nonce         int64    `json:"nonce"`
+	Bits          string   `json:"bits"`
+	Difficulty    int64    `json:"difficulty"`
+	Chainwork     string   `json:"chainwork"`
+	NTx           int64    `json:"n_tx"`
+	Nextblockhash string   `json:"nextblockhash"`
+}
+
+//获取地址信息
+type GetAddressInfo struct {
+	Address             string   `json:"address"`
+	ScriptPubkey        string   `json:"scriptPubKey"`
+	Ismine              bool     `json:"ismine"`
+	solvable            bool     `json:"solvable"`
+	Desc                string   `json:"desc"`
+	Iswatchonly         bool     `json:"iswatchonly"`
+	Isscript            bool     `json:"isscript"`
+	Iswitness           bool     `json:"iswitness"`
+	Witness_version     int64    `json:"witness_version"`
+	Witness_program     string   `json:"witness_program"`
+	Pubkey              string   `json:"pubkey"`
+	Ischange            bool     `json:"ischange"`
+	Timestamp           int64    `json:"timestamp"`
+	Hdkeypath           string   `json:"hdkeypath"`
+	Hdseedid            string   `json:"hdseedid"`
+	Hdmasterfingerprint string   `json:"hdmasterfingerprint"`
+	Lables              []string `json:"lables"`
+}
+
+//获取区块链上的信息
+type GetBlockChainInfo struct {
+	Chain         string `json:"chain"`
+	Blocks        int64  `json:"blocks"`
+	Headers       int64  `json:"headers"`
+	Bestblockhash string `json:"bestblockhash"`
+	Difficulty    int64  `json:"difficulty"`
+}
--- models/user.go
+++ models/user.go
@@ -3,5 +3,5 @@ package models
 type User struct {
 	Id int `form:"id"`
 	UserName string `form:"username"`
-	Possword string `form:"possword"`
+	Password string `form:"possword"`
 }
