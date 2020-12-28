795e936e1e0a2fb611041a690f90d48129ebbaf4
wuyang1232 <1403918572@qq.com>
2020-12-28 11:17:33 +0800

    error change

--- main.go
+++ main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	_ "BTCWebProject/routers"
-	"fmt"
 	"github.com/astaxie/beego"
 )
 
 func main() {
-
 	//静态资源映射文件
 	beego.SetStaticPath("/js","./static/js")
 	beego.SetStaticPath("/img","./static/img")
 	beego.SetStaticPath("/css","./static/css")
-	fmt.Println("hello hanhan")
+
+	//数据库连接
+
 	beego.Run()
 }
 
