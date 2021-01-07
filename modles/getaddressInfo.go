8e5df22a1f5a3fe2e259bef47e0f7388ca638091
JianchengHe <1019015673@qq.com>
2021-01-07 18:15:54 +0800

    gitaddressinfo modify

--- modles/getaddressInfo.go
+++ modles/getaddressInfo.go
@@ -29,11 +29,10 @@ import (
 //将rpc中的数据保存到数据库当中
 func (a AddressInfo)SaveGetAddressInfoData()(int64,error)  {
 	//插入数据
-	result,err :=mysql.DB.Exec("insert into getaddressinfo(address,scriptPubKey,ismine,solvable,desc,iswatchonly,isscript,iswitness,witness_version,witness_program,pubkey,ischange,timestamp,hdkeypath,hdseedid,hdmasterfingerprint)values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-		a.Address,a.ScriptPubkey,a.Ismine,a.solvable,a.Desc,a.Iswatchonly,a.Isscript,a.Iswitness,a.Witness_version,a.Witness_program,a.Pubkey,
-		a.Ischange,a.Timestamp,a.Hdkeypath,a.Hdseedid,a.Hdmasterfingerprint)
+	result,err := mysql.DB.Exec("insert into getaddressinfo(address,scriptPubKey,ismine,solvable,iswatchonly,isscript,iswitness,pubkey,iscompressed,ischange,timestamp,hdkeypath,hdseedid,hdmasterfingerprint)values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+		a.Address,a.ScriptPubkey,a.Ismine,a.solvable,a.Iswatchonly,a.Isscript,a.Iswitness,a.Pubkey,a.Iscompressed,a.Ischange,a.Timestamp,a.Hdkeypath,a.Hdseedid,a.Hdmasterfingerprint)
 	if err != nil {
-		fmt.Println("保存数据失败，请重试", err.Error())
+		fmt.Println("插入数据失败，请重试", err.Error())
 		return -1, err
 	}
 	rows,err :=result.RowsAffected()//影响的行数
@@ -47,9 +46,9 @@ func (a AddressInfo)SaveGetAddressInfoData()(int64,error)  {
 
 //查询数据库中是否有该条记录
 func (a AddressInfo) QueryGetAddressInfoData() (*AddressInfo,error){
-	row := mysql.DB.QueryRow("select address,scriptPubKey,ismine,solvable,desc,iswatchonly,isscript,iswitness,witness_version,witness_program,pubkey,ischange,timestamp,hdkeypath,hdseedid,hdmasterfingerprint from getaddressinfo where address = ?",
+	row := mysql.DB.QueryRow("select address,scriptPubKey,ismine,solvable,desc,iswatchonly,isscript,iswitness,pubkey,iscompressed,ischange,timestamp,hdkeypath,hdseedid,hdmasterfingerprint from getaddressinfo where address = ?",
 		a.Address)
-	err := row.Scan(&a.Address,&a.ScriptPubkey,&a.Ismine,&a.solvable,&a.Desc,&a.Iswatchonly,&a.Isscript,&a.Iswitness,&a.Witness_version,&a.Witness_program,&a.Pubkey,
+	err := row.Scan(&a.Address,&a.ScriptPubkey,&a.Ismine,&a.solvable,&a.Iswatchonly,&a.Isscript,&a.Iswitness,&a.Pubkey,&a.Iscompressed,
 		a.Ischange,&a.Timestamp,&a.Hdkeypath,&a.Hdseedid,&a.Hdmasterfingerprint)
 	if err != nil {
 		fmt.Println("数据查询失败，请重试", err.Error())
